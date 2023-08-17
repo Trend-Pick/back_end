c904c3e218220b21e8ba9bf2dc7b88be563fea0a
chanhee <102280807+chanhee01@users.noreply.github.com>
2023-08-17 22:19:36 +0900

    [Update] 중복 확인기능 추가

---
+++ src/main/java/fashion/look_book/Dto/MyPage/MyPostDto.java
@@ -0,0 +1,17 @@
+package fashion.look_book.Dto.MyPage;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MyPostDto {
+    private String title;
+    private String content;
+    private LocalDateTime time;
+}
