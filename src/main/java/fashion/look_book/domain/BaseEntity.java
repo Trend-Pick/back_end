8f39ab0f6f43fb6f413601456e5c5bd545855a45
chanhee <102280807+chanhee01@users.noreply.github.com>
2023-08-18 14:03:09 +0900

    [Add] BaseEntity(수정기록) 추가

---
+++ src/main/java/fashion/look_book/domain/BaseEntity.java
@@ -0,0 +1,18 @@
+package fashion.look_book.domain;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseEntity {
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+}
